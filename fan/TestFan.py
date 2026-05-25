68085a9cc3e7380e4739e6e441bc2b37ba40894c
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 03:54:01 +0800

    finished TestFan.py

--- fan/TestFan.py
+++ fan/TestFan.py
@@ -1,5 +1,6 @@
 from fan import Fan
 
+# object creation
 fan_1 = Fan()
 fan_2 = Fan()
 
@@ -8,4 +9,18 @@ fan_1.set_speed(Fan.FAST)
 fan_1.set_radius(10)
 fan_1.set_color("yellow")
 fan_1.set_on(True)
-print(fan_1)
+
+# fan_2 assignment of values
+fan_2.set_speed(Fan.MEDIUM)
+fan_2.set_radius(5)
+fan_2.set_color("blue")
+fan_2.set_on(False)
+
+# Comparison
+print(f"""
+{"="*100}
+fan no.1: {fan_1}
+{"-"*100}
+fan no.2: {fan_2}
+{"="*100}
+""")
