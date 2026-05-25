dc6c73fde2234c17d4696ae642054cfdaa3dc78c
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 03:47:09 +0800

    created a program named TestFan and created 2 objects of the Fan class. finished assigning values to first object fan_1

---
+++ fan/TestFan.py
@@ -0,0 +1,11 @@
+from fan import Fan
+
+fan_1 = Fan()
+fan_2 = Fan()
+
+# fan_1 assignment of values
+fan_1.set_speed(Fan.FAST)
+fan_1.set_radius(10)
+fan_1.set_color("yellow")
+fan_1.set_on(True)
+print(fan_1)
