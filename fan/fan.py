105410725662ff6ad9dd06068d06ab1ae613f34e
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 02:41:44 +0800

    created __str__ so the class returns default values and fixed constructor

--- fan/fan.py
+++ fan/fan.py
@@ -1,7 +1,16 @@
 class Fan:
     SLOW, MEDIUM, FAST = 1, 2, 3
-    def __init__(self, speed, radius, color):
-        __speed: int = speed
-        __on: bool = False
-        __radius: float = radius
-        __color: str = color
+    def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
+        self.__speed: int = speed
+        self.__radius: float = radius
+        self.__color: str = color
+        self.__on: bool = on  
+    
+    def __str__(self):
+        return f"speed: {self.__speed} \
+        radius: {self.__radius} \
+        color: {self.__color} \
+        on: {self.__on}"
+
+fan = Fan()
+print(fan)
