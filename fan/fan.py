2c816a560fdb200146e87c7de64a0241937d2bb4
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 03:06:26 +0800

    created the getter and setter for the radius attribute

--- fan/fan.py
+++ fan/fan.py
@@ -15,13 +15,25 @@ class Fan:
     def get_speed(self):
         return self.__speed
     
-    def set_speed(self, value):
-        if value == 1 or 2 or 3:
-            self.__speed: int = value
+    def set_speed(self, speed):
+        if speed == 1 or 2 or 3:
+            self.__speed: int = speed
         else:
             print("Invalid input: options are 1, 2, or 3")
+            
+    def get_radius(self):
+        return self.__radius
+    
+    def set_radius(self, radius):
+        if 0 < radius:
+            self.__radius = radius
+    
+
 
 fan = Fan()
 print(fan)
 fan.set_speed(2)
 print(fan.get_speed())
+
+fan.set_radius(20)
+print(fan.get_radius())
