2dd394b8782eb2ad3ff2e2043543cc51ae83e549
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 20:17:40 +0800

    created setter methods for name, animal type, and age

--- pet/pet.py
+++ pet/pet.py
@@ -1,6 +1,17 @@
 class Pet:
     def __init__(self, name, animal_type, age):
-        __name = name
-        __animal_type = animal_type
-        __age = age
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
     
+    def set_name(self, name):
+        self.__name = name
+    
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        if not (age < 0):
+            self.__age = age
+        else:
+            print("you're pet is not born yet!")
