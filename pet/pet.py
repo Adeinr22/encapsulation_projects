39ca30b528b2607756175603116091161952a871
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 20:39:02 +0800

    created an object of Pet and display its values

--- pet/pet.py
+++ pet/pet.py
@@ -15,3 +15,20 @@ class Pet:
             self.__age = age
         else:
             print("you're pet is not born yet!")
+
+    def get_name(self):
+        return self.__name
+    
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        if not (self.__age < 0):
+            return self.__age
+        else:
+            return 0
+        
+andrei_pet = Pet("Bondat", "Chicken", 1.2)
+print(f"Andrei's pet name is {andrei_pet.get_name()}")
+print(f"Andrei's pet animal is a {andrei_pet.get_animal_type()}")
+print(f"Andrei's pet age is {andrei_pet.get_age()} years old")
