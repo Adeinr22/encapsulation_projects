f8095355d6fa47641da1fcb46cb801fa5cb98ebb
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 23:14:01 +0800

    created test_pet.py to run programs using the Pet class from the pet.py file

--- pet/pet.py
+++ pet/pet.py
@@ -27,8 +27,3 @@ class Pet:
             return self.__age
         else:
             return 0
-        
-andrei_pet = Pet("Bondat", "Chicken", 1.2)
-print(f"Andrei's pet name is {andrei_pet.get_name()}")
-print(f"Andrei's pet animal is a {andrei_pet.get_animal_type()}")
-print(f"Andrei's pet age is {andrei_pet.get_age()} years old")
