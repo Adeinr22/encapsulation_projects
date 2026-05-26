76e53527419067d8db6de2b0c23ce7fb7f7efb1c
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-27 00:00:32 +0800

    created a create_object function for automation. also updated other files

--- car/car.py
+++ car/car.py
@@ -4,6 +4,9 @@ class Car:
         self.__make = make
         self.__speed = 0
     
+    def __str__(self):
+        return f"year model: {self.__year__model} make: {self.__make}"
+    
     def accelerate(self):
         self.__speed += 5
     
@@ -15,3 +18,18 @@ class Car:
     
     def get_speed(self):
         return self.__speed
+    
+    def get_year_model(self):
+        return self.__year__model
+    
+    def get_make(self):
+        return self.__make
+    
+    def set_year_model(self, year_model):
+        if str(year_model).isnumeric():
+            self.__year__model = year_model
+        else:
+            print("invalid input")
+
+    def set_make(self, make):
+        self.__make = make
--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 from fan.fan import Fan
 from car.car import Car
 from pet.pet import Pet
+
+def create_object(class_choice):
+    if class_choice.lower() == "fan":
+        while True:
+            try:
+                speed: str = input("pick fan speed: | (SLOW) (MEDIUM) (FAST) | -> ")
+                radius: float = float(input("input the radius of the fan: "))
+                color: str = input("input the color of the fan: ")
+                on: str = input("turn on the fan? Yes/No | ")
+                if on.lower() == "yes":
+                    on: bool = True
+                elif on.lower() == "no":
+                    on: bool = False
+                else:
+                    on: bool = False
+                    print("invalid input. will stay off")
+                return Fan(speed, radius, color, on)
+            except ValueError:
+                print("invalid input. try again")
+    elif class_choice.lower() == "car":
+        while True:
+            try:
+                year_model: int = input("car year model: ")
+                make: str = input("make: ")
+                return Car(year_model, make)
+            except ValueError:
+                print("invalid input. try again")
+    else:
+        pass
+
+object_1 = create_object("car")
+print(object_1)
