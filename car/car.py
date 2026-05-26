f636d7d5aaaf68434b822f3c40cb7c7bf96b5046
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 23:11:48 +0800

    created a seperate file to run previous program

--- car/car.py
+++ car/car.py
@@ -15,11 +15,3 @@ class Car:
     
     def get_speed(self):
         return self.__speed
-        
-wigo = Car(2020, "Toyota")
-for acc in range(5):
-    wigo.accelerate()
-    print(f"the car accelerates! the speed is now: {wigo.get_speed()}")
-for brk in range(5):
-    wigo.brake()
-    print(f"the car breaks! the speed is now: {wigo.get_speed()}")
