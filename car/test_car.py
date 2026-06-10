ea60007defa645e28d5caa225959320debb26b2a
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-10 23:17:55 +0800

    Rename loop variables for clarity in test_car.py

--- car/test_car.py
+++ car/test_car.py
@@ -1,9 +1,9 @@
 from car import Car
 
 wigo = Car(2020, "Toyota")
-for acc in range(5):
+for accelerate in range(5):
     wigo.accelerate()
     print(f"the car accelerates! the speed is now: {wigo.get_speed()}")
-for brk in range(5):
+for brake in range(5):
     wigo.brake()
     print(f"the car breaks! the speed is now: {wigo.get_speed()}")
